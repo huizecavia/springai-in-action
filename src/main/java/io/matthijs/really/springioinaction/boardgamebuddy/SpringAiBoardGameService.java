64349947cd3e3efa3cde2b038871c3cd78f288a6
m stevens <bomaltennishal@outlook.com>
2025-12-08 12:12:56 +0100

    rules input

--- src/main/java/io/matthijs/really/springioinaction/boardgamebuddy/SpringAiBoardGameService.java
+++ src/main/java/io/matthijs/really/springioinaction/boardgamebuddy/SpringAiBoardGameService.java
@@ -9,9 +9,11 @@ import org.springframework.stereotype.Service;
 public class SpringAiBoardGameService implements BoardGameService {
 
     private final ChatClient chatClient;
+    private final GameRulesService gameRulesService;
 
-    public SpringAiBoardGameService(ChatClient.Builder chatClientBuilder) {
+    public SpringAiBoardGameService(ChatClient.Builder chatClientBuilder, GameRulesService gameRulesService) {
         this.chatClient = chatClientBuilder.build();
+        this.gameRulesService = gameRulesService;
     }
 
     @Value("classpath:/promptTemplates/questionPromptTemplate.st")
@@ -27,7 +29,8 @@ public class SpringAiBoardGameService implements BoardGameService {
                 .user(userSpec -> userSpec
                         .text(questionPromptTemplate)
                         .param("game", question.gameTitle())
-                        .param("question", question.question()))
+                        .param("question", question.question())
+                        .param("rules", gameRulesService.getRulesFor(question.gameTitle())))
                 .call()
                 .content();
 
